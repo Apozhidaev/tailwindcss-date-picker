a910eac39d67e011f99ff8bba111d08c151a999b
alexanderpo <alexander.pozhidaev@gmail.com>
2023-01-06 03:49:59 +0100

    hotfix

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,3 +1,9 @@
+import type {
+  DatePickerOptions,
+  DatePickerInputElement,
+  RangePickerOptions,
+  RangePickerInputElement,
+} from "react-temporal-picker";
 import type { DatePickerProps, RouteDatePickerProps } from "./date";
 import type { RangePickerProps, RouteRangePickerProps } from "./range";
 import RouteDatePicker, { DatePicker } from "./date";
@@ -6,8 +12,12 @@ import RouteRangePicker, { RangePicker } from "./range";
 export type {
   DatePickerProps,
   RangePickerProps,
+  DatePickerOptions,
+  DatePickerInputElement,
   RouteDatePickerProps,
   RouteRangePickerProps,
+  RangePickerOptions,
+  RangePickerInputElement,
 };
 export { DatePicker, RangePicker, RouteDatePicker, RouteRangePicker };
 export default RouteDatePicker;
